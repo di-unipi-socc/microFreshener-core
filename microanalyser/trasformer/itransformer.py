2ad5633b742f347394320cf65f29acc280afb3d7
Davide <davideneri18@gmail.com>
2019-05-04 11:36:31 +0200

    Update itransformer.py

--- microanalyser/trasformer/itransformer.py
+++ microanalyser/trasformer/itransformer.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from ..model.template import MicroModel 
  
 class Trasnformer(ABC):
+ 
     @abstractmethod
-    def load(self, model:MicroModel)->file:str
+    def trasnform(self, model:MicroModel)->file:str
         pass
