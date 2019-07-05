62ca3bb97a89a4b28a3cdfbb94512832c340cbf9
Dido <davide-neri@di.unipi.it>
2019-07-05 13:24:19 +0200

    cli interface with:jsontoyml and ymltojson transformaer

--- microfreshener/command_line.py
+++ microfreshener/command_line.py
@@ -1,20 +1,58 @@
-from microfreshener.core.importer import YMLImporter
+import os
+import click
+import json
+
+from microfreshener.core.importer import JSONImporter, YMLImporter
+from microfreshener.core.exporter import JSONExporter, YMLExporter
 from microfreshener.core.analyser import MicroToscaAnalyserBuilder
 
-import click
-
-@click.command()
-@click.argument('file', type=click.Path(exists=True))
-def main(file):
+@click.group()
+def cli():
     """
-    MicroFreshener-core permits to analyse a microTosca (YML) file and to discover architectural smells
+    MicroFreshener permits to analyse microservice based architectures to discover architectural smells.
     """
     
+
+@cli.command()
+@click.argument('ymlfile', type=click.Path(exists=True))
+def analyse(ymlfile):
+    """
+    Analyse a microTOSCA YML
+    """
     importer = YMLImporter()
-    model = importer.Import(file)
+    model = importer.Import(ymlfile)
     builder = MicroToscaAnalyserBuilder(model)
     analyser = builder.add_all_sniffers().build()
     res = analyser.run()
-    print(res)
+    click.echo(res)
 
     
+@cli.command()
+@click.argument('ymlfile', type=click.Path(exists=True))
+@click.argument('jsonfile')
+def ymltojson(ymlfile, jsonfile):
+    """
+    Transform a microTOSCA YML file to JSON
+    """
+
+    yml_importer = YMLImporter()
+    json_exporter = JSONExporter()
+    mmodel = yml_importer.Import(ymlfile)
+    jsonString = json_exporter.Export(mmodel)
+    with open(jsonfile, 'w') as outfile:
+        json.dump(jsonString, outfile, indent=4)
+
+
+@cli.command()
+@click.argument('jsonfile',type=click.Path(exists=True))
+@click.argument('ymlfile')
+def jsontoyml(jsonfile, ymlfile):
+    """
+    Transform a JSON file into microTosca YML file
+    """
+
+    model = JSONImporter().Import(jsonfile)
+    ymlString = YMLExporter().Export(model)
+    click.echo(ymlString)
+    with open(ymlfile, 'w') as outfile:
+        outfile.write(ymlString) 
