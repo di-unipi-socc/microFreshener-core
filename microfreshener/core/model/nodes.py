cc80166efd7900765f54f999bd54a914a03d7e8e
Dido <davide-neri@di.unipi.it>
2019-08-09 19:49:40 +0200

    cleared nodes module

--- microfreshener/core/model/nodes.py
+++ microfreshener/core/model/nodes.py
@@ -15,32 +15,22 @@ class Root(object):
     def __init__(self, name):
         self.name = name
 
+        # Requirements
+        self._interactions = []
+
         # Incoming interaction of a node
         self.up_interactions = []
 
-        # reverse requirements (deprecated)
-        # self.up_deployment_time_requirements = []
-        # self.up_run_time_requirements = []
-    
     @property
     def interactions(self):
-        return []
-
-    def remove_incoming_relationship(self, relationship):
-        if isinstance(relationship, RunTimeInteraction) and relationship in self.up_run_time_requirements:
-            self.up_run_time_requirements.remove(relationship)
-        if isinstance(relationship, DeploymentTimeInteraction) and relationship in self.up_deployment_time_requirements:
-            self.up_deployment_time_requirements.remove(relationship)
-   
-    def set_incoming_interaction(self, interaction):
-        self.up_interactions.append(interaction)
+        return self._interactions
 
     @property
     def incoming_interactions(self):
         return self.up_interactions
     
-    # Add a interactWith interaction from the node to a target node,
-    # the Interact With Relations check the cooret source (only service and MeesagRoouter can have interactions)
+    # Add a interactWith interaction from source node to target node.
+    # Only Service and MessagRoouter can be source of a relation (checkd in the InteractWith costructor)
     def add_interaction(self, item, with_timeout=False, with_circuit_breaker=False, with_dynamic_discovery=False):
         if not isinstance(item, InteractsWith):
             item = InteractsWith(self, item, with_timeout=with_timeout,
@@ -48,22 +38,15 @@ class Root(object):
                                     with_dynamic_discovery=with_dynamic_discovery)
         self._interactions.append(item)
         if not isinstance(item.target, str):
-            item.target.set_incoming_interaction(item)
+            item.target.add_incoming_interaction(item)
         return item
+   
+    def add_incoming_interaction(self, interaction):
+        self.up_interactions.append(interaction)
 
-
-
-    # @property
-    # def incoming(self):
-    #     return self.up_deployment_time_requirements + self.up_run_time_requirements
-
-    # @property
-    # def incoming_run_time(self):
-    #     return self.up_run_time_requirements
-
-    # @property
-    # def incoming_deployment_time(self):
-    #     return self.up_deployment_time_requirements
+    def remove_incoming_relationship(self, relationship):
+        if  relationship in self.up_interactions:
+            self.up_interactions.remove(relationship)
 
     def __str__(self):
         return self.name
@@ -83,88 +66,12 @@ class Software(Root):
     def __init__(self, name):
         super(Software, self).__init__(name)
 
-        # (deprecated) requirements
-        # self._run_time = []
-        # self._deployment_time = []
-
-
-
-    # @property
-    # def relationships(self):
-    #     return self._run_time + self._deployment_time
-    
-    # @property
-    # def run_time(self):
-    #     return (i.format for i in self._run_time)
-
-    # @property
-    # def deployment_time(self):
-    #     return (i.format for i in self._deployment_time)
-
-
-    # # Depracted
-    # def add_run_time(self, item, with_timeout=False, with_circuit_breaker=False, with_dynamic_discovery=False):
-    #     logger.debug("{}: adding runtime link to {}".format(self, item))
-    #     if not isinstance(item, RunTimeInteraction):
-    #         item = RunTimeInteraction(self, item, with_timeout=with_timeout,
-    #                                   with_circuit_breaker=with_circuit_breaker,
-    #                                   with_dynamic_discovery=with_dynamic_discovery)
-    #     self._run_time.append(item)
-    #     if not isinstance(item.target, str):
-    #         item.target.up_run_time_requirements.append(item)
-
-    # # Depracted
-    # def add_deployment_time(self, item, with_timeout=False, with_circuit_breaker=False, with_dynamic_discovery=False):
-    #     logger.debug("{}: adding deployment link to {}".format(self, item))
-    #     if not isinstance(item, DeploymentTimeInteraction):
-    #         item = DeploymentTimeInteraction(self, item, with_timeout=with_timeout,
-    #                                          with_circuit_breaker=with_circuit_breaker,
-    #                                          with_dynamic_discovery=with_dynamic_discovery)
-    #     self._deployment_time.append(item)
-    #     if not isinstance(item.target, str):
-    #         item.target.up_deployment_time_requirements.append(item)
-
 
 class Service(Software):
 
     def __init__(self, name):
         super(Service, self).__init__(name)
-        # Requirements
-        self._interactions = []
 
-        # requirements
-        # self._run_time = []
-        # self._deployment_time = []
-    @property
-    def interactions(self):
-        return self._interactions
-
-    # def add_interaction(self, item, with_timeout=False, with_circuit_breaker=False, with_dynamic_discovery=False):
-    #     if not isinstance(item, InteractsWith):
-    #         item = InteractsWith(self, item, with_timeout=with_timeout,
-    #                                 with_circuit_breaker=with_circuit_breaker,
-    #                                 with_dynamic_discovery=with_dynamic_discovery)
-    #     self._interactions.append(item)
-    #     if not isinstance(item.target, str):
-    #         item.target.set_incoming_interaction(item)
-    #     return item
-    @property 
-    def interactions(self):
-        return self._interactions
-
- 
-  
-    # @property
-    # def relationships(self):
-    #     return self._run_time + self._deployment_time
-
-    # @property
-    # def run_time(self):
-    #     return self._run_time
-
-    # @property
-    # def deployment_time(self):
-    #     return self._deployment_time
 
     def __str__(self):
         return '{} ({})'.format(self.name, 'service')
@@ -174,28 +81,11 @@ class CommunicationPattern(Software):
 
     def __init__(self, name, short_name="CP"):
         super(CommunicationPattern, self).__init__(name)
-
         self.short_name = short_name
-        # requirements
-        # self._run_time = []
-        # self._deployment_time = []
-
-    # @property
-    # def relationships(self):
-    #     return self._run_time + self._deployment_time
-
-    # @property
-    # def run_time(self):
-    #     return self._run_time
-
-    # @property
-    # def deployment_time(self):
-    #     return self._deployment_time
 
     def __str__(self):
         return '{} ({})'.format(self.name, self.short_name)
     
-
 class MessageBroker(CommunicationPattern):
 
     def __init__(self, name):
@@ -208,39 +98,11 @@ class MessageRouter(CommunicationPattern):
 
     def __init__(self, name):
         super(MessageRouter, self).__init__(name, "MR")
-        self._interactions = []
-    
-    # def add_interaction(self, item, with_timeout=False, with_circuit_breaker=False, with_dynamic_discovery=False):
-    #     if not isinstance(item, InteractsWith):
-    #         item = InteractsWith(self, item, with_timeout=with_timeout,
-    #                                 with_circuit_breaker=with_circuit_breaker,
-    #                                 with_dynamic_discovery=with_dynamic_discovery)
-    #     self._interactions.append(item)
-    #     if not isinstance(item.target, str):
-    #         item.target.set_incoming_interaction(item)
-    #     return item
-    
-    @property
-    def interactions(self):
-        return self._interactions
-
 
 class Database(Root):
 
     def __init__(self, name):
         super(Database, self).__init__(name)
-
-    # @property
-    # def relationships(self):
-    #     return []
-
-    # @property
-    # def run_time(self):
-    #     return []
-
-    # @property
-    # def deployment_time(self):
-    #     return []
 
     def __str__(self):
         return '{} ({})'.format(self.name, 'database')
